33185c7f70a5f43f5e29c51227cfdbf9a6b8d35e
BazimAzeem <bazim.azeem@outlook.com>
2023-04-09 10:54:09 -0400

    Created unti test project

---
+++ PackageRegistry.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace PackageRegistry.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
