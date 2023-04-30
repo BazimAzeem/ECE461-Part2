e289dbda656a5758b9520409630e5ee7d5c6f084
BazimAzeem <bazim.azeem@outlook.com>
2023-04-30 16:17:06 -0400

    Testing new build 2

--- PackageRegistry/Controllers/DefaultApi.cs
+++ PackageRegistry/Controllers/DefaultApi.cs
@@ -128,7 +128,7 @@ namespace PackageRegistry.Controllers
             // return StatusCode(400);
 
             //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..),-...
-            // return StatusCode(404);
+            return StatusCode(404);
 
             //TODO: Uncomment the next line to return response 0 or use other options such as return this.NotFound(), return this.BadRequest(..),-...
             // return StatusCode(0, default(Error));
