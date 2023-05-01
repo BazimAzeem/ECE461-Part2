bde06512fd9be64707759d31b09792bc3c113265
BazimAzeem <bazim.azeem@outlook.com>
2023-04-30 22:01:06 -0400

    fixed logging id

--- PackageRegistry/Program.cs
+++ PackageRegistry/Program.cs
@@ -30,7 +30,7 @@ namespace PackageRegistry
 
         public static int Main(String[] args)
         {
-           
+
             new Program(args);
             return ProgramStatus;
 
@@ -57,38 +57,41 @@ namespace PackageRegistry
             {
                 LOG_FILE = log_file_env_var;
             }
-			
-			Console.WriteLine(LogSeverity.Debug);
 
-			// Test metric stuff
-			var calculator1 = new MetricsCalculation.MetricsCalculator("https://github.com/BazimAzeem/ECE461-Part2");
-			var calculator2 = new MetricsCalculation.MetricsCalculator("https://github.com/npm/cli");
-			var calculator3 = new MetricsCalculation.MetricsCalculator("https://github.com/microsoft/vscode");
-			var calculator4 = new MetricsCalculation.MetricsCalculator("https://github.com/shinout/browser");
-			calculator1.Calculate();
-			calculator2.Calculate();
-			calculator3.Calculate();
-			calculator4.Calculate();
-			LogInfo(calculator1.ToString());
-			LogInfo(calculator2.ToString());
-			LogInfo(calculator3.ToString());
-			LogInfo(calculator4.ToString());
+            Console.WriteLine(LogSeverity.Debug);
 
-			
+            // Test metric stuff
+            var calculator1 = new MetricsCalculation.MetricsCalculator("https://github.com/BazimAzeem/ECE461-Part2");
+            var calculator2 = new MetricsCalculation.MetricsCalculator("https://github.com/npm/cli");
+            var calculator3 = new MetricsCalculation.MetricsCalculator("https://github.com/microsoft/vscode");
+            var calculator4 = new MetricsCalculation.MetricsCalculator("https://github.com/shinout/browser");
+            calculator1.Calculate();
+            calculator2.Calculate();
+            calculator3.Calculate();
+            calculator4.Calculate();
+            LogInfo(calculator1.ToString());
+            LogInfo(calculator2.ToString());
+            LogInfo(calculator3.ToString());
+            LogInfo(calculator4.ToString());
 
-			try {
-           		CreateWebHostBuilder(args).Build().Run();
-			}catch (Exception ex) {
-				string logMessage = string.Format("Exception message: {0}\nException type: {1}\nStack trace:\n{2}",
-					ex.Message, ex.GetType().FullName, ex.StackTrace);
-				if (ex.InnerException != null)
-				{
-					logMessage += string.Format("\n\nInner exception message: {0}\nInner exception type: {1}\nInner exception stack trace:\n{2}",
-						ex.InnerException.Message, ex.InnerException.GetType().FullName, ex.InnerException.StackTrace);
-				}
-			
-				LogError("Unexcepted exception occured in CreateWebHostBuilder " + logMessage);
-			}
+
+
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                string logMessage = string.Format("Exception message: {0}\nException type: {1}\nStack trace:\n{2}",
+                    ex.Message, ex.GetType().FullName, ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    logMessage += string.Format("\n\nInner exception message: {0}\nInner exception type: {1}\nInner exception stack trace:\n{2}",
+                        ex.InnerException.Message, ex.InnerException.GetType().FullName, ex.InnerException.StackTrace);
+                }
+
+                LogError("Unexcepted exception occured in CreateWebHostBuilder " + logMessage);
+            }
         }
 
         /// <summary>
@@ -101,24 +104,25 @@ namespace PackageRegistry
         .UseStartup<Startup>();
 
 
-        
 
-		public void AppendToLog() {
+
+        public void AppendToLog()
+        {
             // append to the log file when we destroy this class
             try
             {
                 File.AppendAllText(LOG_FILE, log.ToString());
-				log.Clear();
-			}
+                log.Clear();
+            }
             catch (DirectoryNotFoundException)
             {
                 LogWarning("Log file directory not found. Check your LOG_FILE environment variable");
             }
-		}
+        }
 
-		~Program()
+        ~Program()
         {
-			AppendToLog();
+            AppendToLog();
         }
 
 
@@ -138,14 +142,14 @@ namespace PackageRegistry
                 log.AppendLine(outmsg);
             }
 
-			WriteLogEntry("[ERROR]", msg);
+            WriteLogEntry("ERROR", msg);
 
 
         }
         public static void LogWarning(string msg)
         {
 
-            string outmsg = "[WARNING] " + msg;
+            string outmsg = "WARNING" + msg;
 
             if (LOG_LEVEL >= 1)
             {
@@ -156,7 +160,7 @@ namespace PackageRegistry
                 log.AppendLine(outmsg);
             }
 
-			WriteLogEntry("[WARNING]", msg);
+            WriteLogEntry("WARNING", msg);
         }
 
         public static void LogInfo(string msg)
@@ -170,7 +174,7 @@ namespace PackageRegistry
                 log.AppendLine(outmsg);
             }
 
-			WriteLogEntry("[INFO]", msg);
+            WriteLogEntry("INFO", msg);
         }
 
         public static void LogDebug(string msg)
